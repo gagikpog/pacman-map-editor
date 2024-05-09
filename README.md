940023a6838c60d401b17ac83d5878ab10e6a929
Gagik <gagikpog@gmail.com>
2024-05-09 09:59:19 +0500

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# PackMan map editor
---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/tailwind.min.css';
+import Header from '../components/header';
+
+export default function App() {
+    return (
+        <>
+            <Header/>
+            <main>
+                content
+            </main>
+        </>
+    );
+}
