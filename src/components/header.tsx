59a23085c415cf407b2fc77ad82d65d5f168747f
Gagik <gagikpog@gmail.com>
2024-05-11 11:27:23 +0500

    add icons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
+import { Icon } from './icon';
+
 export default function Header() {
     return (
-        <header className="tw-flex tw-bg-slate-700 tw-p-2 tw-text-white tw-sticky tw-top-0">
-            <div> PMME </div>
+        <header className="tw-flex tw-bg-slate-700 tw-p-2 tw-text-white tw-sticky tw-top-0 tw-items-center">
+            <Icon icon="logo" type="png" size={32}/>
+            <div className="tw-ml-2 tw-uppercase"> PacMan map editor</div>
         </header>
     );
 }
--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -2,17 +2,19 @@ interface IIconProps extends IProps {
     icon: string;
     selected?: boolean;
     title?: string;
+    type?: string;
+    size?: number;
     onClick?: () => void;
 }
 
-export const Icon = ({icon, selected, onClick, className, title}: IIconProps) => {
+export const Icon = ({icon, selected, onClick, className, title, type = 'jpg', size = 16}: IIconProps) => {
     return (
         <img
-            src={`/assets/${icon}.jpg`}
+            src={`/assets/${icon}.${type}`}
             className={`tw-cursor-pointer ${ selected ? 'icon-selected' : ''} ${className || ''}`}
             alt={icon}
-            width={16}
-            height={16}
+            width={size}
+            height={size}
             title={title}
             onClick={onClick}
         />
